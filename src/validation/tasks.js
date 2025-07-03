33a1fd0932923111fb6f36b108386e6cdf5ef955
Anastasia_S <sivokonanastasia1@gmail.com>
2025-07-04 00:03:52 +0300

    ain

--- src/validation/tasks.js
+++ src/validation/tasks.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const createTaskSchema = Joi.object({
   title: Joi.string().max(30).required(),
-  description: Joi.string().max(60).allow("").optional(), // ✅ виправлено
+  description: Joi.string().max(60).allow("").optional(),
   completed: Joi.boolean()
 });
 
